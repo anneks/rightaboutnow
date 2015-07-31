3cd7ad1683b011c391c27515869505659a616ace
Patrick <pingvinen@isflagen.dk>
2015-07-31 14:01:26 +0200

    Added automatic handling of different input types (e.g. array to print_r)

--- src/helpers/DebugLog.class.php
+++ src/helpers/DebugLog.class.php
@@ -2,8 +2,31 @@
 
 class DebugLog
 {
-	public static function log($msg, $file = '/tmp/debuglog')
+	private static function write($msg, $file)
 	{
 		file_put_contents($file, $msg . "\n", FILE_APPEND);
+	}
+
+	public static function log($msg, $file = '/tmp/debuglog')
+	{
+		if (is_array($msg))
+		{
+			self::write(print_r($msg, true), $file);
+		}
+
+		elseif (is_null($msg))
+		{
+			self::write('null', $file);
+		}
+
+		elseif (is_bool($msg))
+		{
+			self::write($msg ? 'true' : 'false', $file);
+		}
+
+		else
+		{
+			self::write($msg, $file);
+		}
 	}
 }
